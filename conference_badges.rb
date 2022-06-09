0b3c26a3032b937e8bf63fd0b10dfc043b3ea685
Mwagiru <joymwagiru@gmail.com>
2022-06-09 08:32:54 +0300

    commit tree 0864f759f2015eded081e1b9b2a7c8668fc8d1e0

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker(name)
+     "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(names)
+    names.map.with_index(1) do|name, num|
+        "Hello, #{name}! Your room number is #{num}"
+    end
+end
+
+def printer(names)
+  batch_badge_creator(names).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(names).each do |assign|
+    puts assign
+  end
+end
